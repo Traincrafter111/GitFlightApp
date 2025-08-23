ab331f50db2cfd4dcf021b89b51d6a8aa327565e
Traincrafter111 <traincrafter111@gmail.com>
2025-08-23 09:48:21 -0600

    Add Initial MAUI Project Structure

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GitFlightApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace GitFlightApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
