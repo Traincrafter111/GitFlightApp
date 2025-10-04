633d9bd16a1ae14b79c8c5f3769b4ba5811753fb
Traincrafter111 <traincrafter111@gmail.com>
2025-10-04 08:50:12 -0600

    Added CreatedAt Logic

--- Models/Connection.cs
+++ Models/Connection.cs
@@ -12,6 +12,8 @@ namespace GitFlightApp.Models
         public string Airline { get; set; } = string.Empty;
         public string DepartureCity { get; set; } = string.Empty;
         public string ArrivalCity { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public string Description { get; set; } = string.Empty;
         public ICollection<FlightConnection> FlightConnections { get; set; } = [];
     }
 }
